0661fbcb855de83d2f92f3b8cf122cd8793138c2
sumail <sumail@xyzzdev.com>
2026-01-29 16:44:45 +0800

    project link

--- src/constants/index.js
+++ src/constants/index.js
@@ -216,7 +216,7 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/miraclesumail/rent-a-car",
   },
   {
     name: "Job IT",
