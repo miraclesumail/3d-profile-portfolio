1c2c09755a6d3566087e3628b9b711603f15112f
sumail <sumail@xyzzdev.com>
2026-01-30 20:29:31 +0800

    test builld mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+/*
+ * @Author: sumail sumail@xyzzdev.com
+ * @Date: 2024-10-29 17:21:28
+ * @LastEditors: sumail sumail@xyzzdev.com
+ * @LastEditTime: 2026-01-30 20:28:54
+ * @FilePath: /project_3D_developer_portfolio-main/src/App.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEv
+ */
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
+console.log(import.meta.env.MODE, 'MODE--')
 const App = () => {
   return (
     <BrowserRouter>
